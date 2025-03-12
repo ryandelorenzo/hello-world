dcca74725b2fdd5b74b65ea7a1eb634e92f2c32f
ryandelorenzo <ryandeloren@gmail.com>
2025-03-12 17:12:24 -0400

    built docs location

---
+++ docs/browser/chunk-6SEVXOCJ.js
@@ -0,0 +1,3 @@
+import{Ba as e,qa as n,xa as s,ya as p}from"./chunk-MZ7E6M6W.js";var i=class t{static \u0275fac=function(o){return new(o||t)};static \u0275cmp=n({type:t,selectors:[["app-todos"]],decls:3,vars:0,template:function(o,m){o&1&&(s(0,"p"),e(1,"todos works!"),p(),e(2,`
+you are not in todos
+`))},encapsulation:2})};export{i as TodosComponent};
