c440bddc745437c75aea42e3783f8f24a2e6a12d
ryandelorenzo <ryandeloren@gmail.com>
2025-03-12 17:09:43 -0400

    inital commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+      path: '',
+      pathMatch: 'full',
+      loadComponent: () => {
+        return import('./components/home/home.component').then((m) => m.HomeComponent);
+      },
+    }, 
+    {
+        path: 'todos',
+        loadComponent: () => {
+            return import('./components/todos/todos.component').then((m) => m.TodosComponent);
+        }
+    },
+];
